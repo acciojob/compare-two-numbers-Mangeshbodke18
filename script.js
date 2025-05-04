a5c2b76cf847368743524dcc799ff253acb55175
Mangesh Bodke <mangeshbodke582@gmail.com>
2025-05-04 12:06:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 //your JS code here. If required.
 
-//let a=prompt("a-");
-//let b=prompt("b-");
+let a=prompt("a-");
+let b=prompt("b-");
 
 function abc(a,b){
 
 	if(a==b){
 		return true;
 }
+		
 	else{
 return false;
 	}
 }
 
 
-alert(abc(4,6));
+alert(abc(a,b));
